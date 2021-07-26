01942650736416baaacb0a499a3f09c31f7d04d1
Maryna Danylchuk <marina123456mari@gmail.com>
2021-07-26 15:58:26 +0300

    1

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const exhbs = require('express-handlebars')
+const products = require('./products.json')
+const app = express();
+
+
+app.use(express.static('public'));
+app.set('view engine', 'hbs');
+app.engine('hbs',
+    exhbs({
+extname: 'hbs',
+}));
+//=============cтатический параметр============
+app.get('/', (req, res) => {
+    res.render('home');
+    
+});
+app.get('/products', (req, res) => {
+    res.render('products', {products, cssFileName: "products" });
+})
+//==============динамический параметр========
+app.get('/product/:productId', (req, res) => {
+    console.log(req.params);
+    const product = products.find(p => p.id === req.params.productId);
+    res.render('product', {product})
+});
+
+
+
+app.listen(444, () => {
+    console.log('ejjjye ydhtpopj ${444}')
+});
